622fd1692e4cff69c9264a994a25621ecbccaddf
luccaspg <luccaspg@gmail.com>
2019-11-23 22:35:20 -0500

    fixing ui

--- src/components/NestedList.js
+++ src/components/NestedList.js
@@ -1,6 +1,7 @@
+  
 import React from "react";
-import "./NestedList.css";
-import pokemon from "../pokemon.json";
+// import "./NestedList.css";
+import pokemon from "./pokemon.json";
 import { makeStyles } from "@material-ui/core/styles";
 import ListSubheader from "@material-ui/core/ListSubheader";
 import List from "@material-ui/core/List";
@@ -13,6 +14,12 @@ import ExpandLess from "@material-ui/icons/ExpandLess";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import StarBorder from "@material-ui/icons/StarBorder";
 import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -25,19 +32,36 @@ const useStyles = makeStyles(theme => ({
     }
 }));
 
-export default function NestedList() {
+export default function Blockchain() {
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
+    const [trade, setTrade] = React.useState(false);
 
-    const handleClick = () => {
+    const handleOpen = () => {
         setOpen(!open);
     };
+    const openTrade = () => {
+        setOpenTrade(!openTrade);
+      };
+
+    function onSendMessage(message) {
+        if (!message)
+          return
+    
+        this.props.onSendMessage(message, (err) => {
+          if (err)
+            return console.error(err)
+    
+        //   return setMessage({ message: '' })
+        })
+    }
 
     const handleTrade = () => {};
 
     const users = pokemon.people.map(user => {
         const pokeTemp = user.pokemon.map(poke => {
             return (
+                <div>
                 <ListItem button className={classes.nested}>
                     <ListItemIcon>
                         <StarBorder />
@@ -46,16 +70,41 @@ export default function NestedList() {
                     <Button
                         variant="contained"
                         color="primary"
-                        onClick={handleTrade}
+                        onClick={openTrade}
                     >
                         Trade
                     </Button>
                 </ListItem>
+                <Dialog open={open} onClose={openTrade} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Trade</DialogTitle>
+                    <DialogContent>
+                    <DialogContentText>
+                        Send a message along with your trade request
+                    </DialogContentText>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Email Address"
+                        type="email"
+                        fullWidth
+                    />
+                    </DialogContent>
+                    <DialogActions>
+                    <Button onClick={handleCloseTrade} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleCloseTrade} color="primary">
+                        Send Request FIX THE FUNC
+                    </Button>
+                    </DialogActions>
+                </Dialog>
+                </div>
             );
         });
         return (
             <div>
-                <ListItem button onClick={handleClick}>
+                <ListItem button onClick={handleOpenTrade}>
                     <ListItemIcon>
                         <SendIcon />
                     </ListItemIcon>
@@ -83,57 +132,6 @@ export default function NestedList() {
             className={classes.root}
         >
             {users}
-            {/* <ListItem button onClick={handleClick}>
-                <ListItemIcon>
-                    <SendIcon />
-                </ListItemIcon>
-                <ListItemText primary="User 1" />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText primary="Starred" />
-                    </ListItem>
-                </List>
-            </Collapse>
-            <ListItem button onClick={handleClick}>
-                <ListItemIcon>
-                    <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="User 2" />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText primary="Starred" />
-                    </ListItem>
-                </List>
-            </Collapse>
-            <ListItem button onClick={handleClick}>
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="User 3" />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItem>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
-                        <ListItemText primary="Starred" />
-                    </ListItem>
-                </List>
-            </Collapse> */}
         </List>
     );
 }
