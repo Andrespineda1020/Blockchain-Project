2771735585f6fe2671bb97aef4d6b24f0ae666d2
luccaspg <luccaspg@gmail.com>
2019-11-26 20:07:29 -0500

    ignore this one

---
+++ src/components/TradeDialog.js
@@ -0,0 +1,39 @@
+// import React from "react";
+// import Button from "@material-ui/core/Button";
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import TextField from '@material-ui/core/TextField';
+
+// export default function TradeDialog(props) {
+
+
+//     return (
+//         <Dialog open={trade} onClose={handleTrade} aria-labelledby="form-dialog-title">
+//             <DialogTitle id="form-dialog-title">Trade</DialogTitle>
+//             <DialogContent>
+//             <DialogContentText>
+//                 Send a message along with your trade request
+//             </DialogContentText>
+//             <TextField
+//                 autoFocus
+//                 margin="dense"
+//                 id="name"
+//                 label="Email Address"
+//                 type="email"
+//                 fullWidth
+//             />
+//             </DialogContent>
+//             <DialogActions>
+//             <Button onClick={handleTrade} color="primary">
+//                 Cancel
+//             </Button>
+//             <Button onClick={handleTrade} color="primary">
+//                 Send Request
+//             </Button>
+//             </DialogActions>
+//         </Dialog>
+//     );
+// };
